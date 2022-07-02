298cd5cdf6f14dbc45dfc3f02fac1f7d017a444b
marcoschultznunes <marco_sn@hotmail.com>
2022-07-02 20:00:54 -0300

    Project structure

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,14 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { UsersListComponent } from './users-list/users-list.component';
+import { UserFormComponent } from './user-form/user-form.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    UsersListComponent,
+    UserFormComponent
   ],
   imports: [
     BrowserModule
